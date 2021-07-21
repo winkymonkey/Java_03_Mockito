972afc8afc85401e1dc183296e81b4bd4feb8ac6
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 00:53:23 +0530

    refactor

--- src/test/java/org/example/tdd/BookDao.java
+++ src/test/java/org/example/tdd/BookDao.java
@@ -1,4 +1,4 @@
-package com.example.tdd;
+package org.example.tdd;
 
 import java.util.Collections;
 import java.util.List;
--- src/test/java/org/example/tdd/BookDaoTest.java
+++ src/test/java/org/example/tdd/BookDaoTest.java
@@ -1,4 +1,4 @@
-package com.example.tdd;
+package org.example.tdd;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
